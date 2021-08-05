7c5371477b9c2bd417e96243a28207de3df2dbd6
Jordan Leven <jordan@heysparkbox.com>
2021-08-05 16:56:18 -0400

    DROP: VALIDATION

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,13 @@ import '../css/index.css';
 const helloWorld = () => console.log('Hello!');
 
 helloWorld();
+
+const ws = new WebSocket('ws://localhost:3000/');
+
+ws.onmessage = (event) =>{
+  console.log(`Received message: ${event.data}`);
+};
+
+document.body.addEventListener('click', () => {
+  ws.send("Well hello there!");
+});
