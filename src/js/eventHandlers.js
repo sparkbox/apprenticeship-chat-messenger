a2329a665645a6c36bb6e3652869b6a9bdfa72bb
Angus Chang <angus@heysparkbox.com>
2021-08-09 09:11:54 -0400

    refactor: change eventHandlers to accept callbacks
    eventHandler functions now accept callbacks as a parameter
    
    FSA21V2-62

--- src/js/eventHandlers.js
+++ src/js/eventHandlers.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export const listenForKeypressAndConsoleMessage = () => {
   console.log(document.body);
   document.addEventListener('DOMContentLoaded', () => {
@@ -10,17 +11,26 @@ export const listenForKeypressAndConsoleMessage = () => {
         textArea.value = '';
       }
     });
+=======
+export const callbackWhenEnterIsPressed = (callback) => {
+  const textArea = document.querySelector('.chat-window__input');
+  textArea.addEventListener('keypress', (e) => {
+    // if key is 'Enter' key console message
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      callback(textArea.value);
+      textArea.value = '';
+    }
+>>>>>>> f743a81 (refactor: change eventHandlers to accept callbacks)
   });
 };
 
-export const listenForSubmitAndConsoleMessage = () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    const chatForm = document.querySelector('.chat-window__sender-features');
-    const textArea = document.querySelector('.chat-window__input');
-    chatForm.addEventListener('submit', (e) => {
-      e.preventDefault();
-      console.log(textArea.value);
-      textArea.value = '';
-    });
+export const callbackOnFormSubmit = (callback) => {
+  const chatForm = document.querySelector('.chat-window__sender-features');
+  const textArea = document.querySelector('.chat-window__input');
+  chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    callback(textArea.value);
+    textArea.value = '';
   });
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import '../css/index.css';
+import { addMessageToChatHistory } from './chatWindow';
+import { callbackWhenEnterIsPressed, callbackOnFormSubmit } from './eventHandlers';
 
 const helloWorld = () => console.log('Hello!');
+
+callbackWhenEnterIsPressed(addMessageToChatHistory);
+callbackOnFormSubmit(addMessageToChatHistory);
 
 helloWorld();
