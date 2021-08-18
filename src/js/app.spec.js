0ed54c4ce4ae07353370ebd34f34986eda2d9350
Angus Chang <angus@heysparkbox.com>
2021-08-18 17:14:08 -0400

    test: test app.js
    add tests to app.spec.js to make sure all actions
    get run when we call main.
    
    FSA21V2-62

--- src/js/app.spec.js
+++ src/js/app.spec.js
@@ -2,26 +2,19 @@
  * @jest-environment jsdom
  */
 import main from './app.js';
+import { addMessageToChatHistory } from './chatWindow.js';
+import { onEnterPress, onFormSubmit } from './eventHandlers.js';
 
-document.body.innerHTML = `<div class="chat-window">
-<div class="js-add-message-to-chat-history chat-window__messages">
-<div class="chat-window__message chat-window__message--incoming">hi</div>
-<div class="chat-window__message chat-window__message--outgoing">hello</div>
-<div class="chat-window__message chat-window__message--outgoing">hows it going?</div>
-<div class="chat-window__message chat-window__message--incoming">not bad</div>
-</div>
-<form class="chat-window__sender-features">
-  <label for="message-area" class="visually-hidden">Message area</label>
-  <textarea
-    name="message-area"
-    id="message-area"
-    class="chat-window__input"></textarea>
-  <button class="chat-window__send-button" type="submit">Send</button>
-</form>
-</div>`;
+jest.mock('./eventHandlers.js');
+jest.mock('./chatWindow.js');
 
 describe('app.js', () => {
-  it('A good place for a test', () => {
+  it('Runs onEnterPress with addMessageToChatHistory as a parameter', () => {
     main();
+    expect(onEnterPress).toHaveBeenCalledWith(addMessageToChatHistory);
+  });
+  it('Runs onFormSubmit with addMessageToChatHistory as a parameter', () => {
+    main();
+    expect(onFormSubmit).toHaveBeenCalledWith(addMessageToChatHistory);
   });
 });
